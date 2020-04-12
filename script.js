c72302cb7c6007248707aa13d79b29628a1ab483
Zee-Nana <60174809+Zee-Nana@users.noreply.github.com>
2020-04-12 15:31:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,150 @@
+new Vue({
+    el: "#page",
+
+    data() {
+        return {
+        // hello: "how are you",
+        cards:[
+            {
+                id: 1,
+                title: 'kjhgfdsdfgh',
+                image: ('hand.jpg'),
+                text: 'make sure you wash your hands regularly for atleast 20seconds under running water with soap'
+            },
+            {
+                id: 2,
+                title: 'kjhgfdsdfgh',
+                image: ('sanitizer.png'),
+                text: 'if no water and soap is available use an alcohol-based sanitiser to rub your hands. '
+            },
+            {
+                id: 3,
+                title: 'kjhgfdsdfgh',
+                image: ('mask.jpg'),
+                text: 'make sure you use nose mask incase you want to come in contact with person'
+            },
+            {
+                id: 4,
+                title: 'kjhgfdsdfgh',
+                image: ('social.jpg'),
+                text: ' Avoiding/postponing events with large gatherings of people. '
+            },
+            {
+                id: 5,
+                title: 'kjhgfdsdfgh',
+                image: ('anti.webp'),
+                text: 'clean your Enviroment Regularly using Disinfectant Spray'
+            },
+            {
+                id: 6,
+                title: 'kjhgfdsdfgh',
+                image: ('face.webp'),
+                text: 'Avoid face contact with your palm, instead use your arm'
+            }
+        ],
+        nums:[
+            {
+                id: 1,
+                state: "FCT",
+                total: 56
+            },
+            {
+                id: 2,
+                state: "Lagos",
+                total: 174
+            },
+            {
+                id: 3,
+                state: "Ogun",
+                total: 7
+            },
+            {
+                id: 4,
+                state: "Oyo",
+                total: 11
+            },
+            {
+                id: 5,
+                state: "Enugu",
+                total: 2
+            },
+            {
+                id: 6,
+                state: "Akwa Ibom",
+                total: 5
+            },
+            {
+                id: 7,
+                state: "Ekiti",
+                total: 1 
+            },
+            {
+                id: 8,
+                state: "Edo",
+                total: 12
+            },
+            {
+                id: 9,
+                state: "Benue",
+                total: 2
+            },
+            {
+                id: 10,
+                state: "Kaduna",
+                total: 6
+            },
+            {
+                id: 11,
+                state: "Kano",
+                total: 1
+            },
+            {
+                id: 12,
+                state: "Osun",
+                total: 20
+            },
+            {
+                id: 13,
+                state: "Bauchi",
+                total: 8
+            },
+            {
+                id: 14,
+                state: "Delta",
+                total: 3
+            },
+            {
+                id: 15,
+                state: "Kwara",
+                total: 2
+            },
+            {
+                id: 16,
+                state: "Ondo",
+                total: 2
+            },
+            {
+                id: 17,
+                state: "Niger",
+                total: 1
+            },
+            {
+                id: 18,
+                state: "Anambra",
+                total: 1
+            },
+            {
+                id: 19,
+                state: "Rivers",
+                total: 2
+            },
+            {
+                id: 20,
+                state: "Katsina",
+                total: 4
+            },
+
+        ]
+    }
+}
+})
